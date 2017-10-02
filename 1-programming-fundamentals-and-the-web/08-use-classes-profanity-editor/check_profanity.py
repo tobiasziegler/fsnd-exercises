e9637713202ec18078c5eda66bb81cd3ed567b99
David Mallard <correspondence@davidmallard.id.au>
2017-10-02 18:20:38 +1100

    Complete the Accessing A Website With Code exercise

--- 1-programming-fundamentals-and-the-web/08-use-classes-profanity-editor/check_profanity.py
+++ 1-programming-fundamentals-and-the-web/08-use-classes-profanity-editor/check_profanity.py
@@ -1,7 +1,16 @@
+import urllib
+
 def read_text():
     quotes = open("/Users/tobiasziegler/Projects/udacity/fsnd/fsnd-exercises/1-programming-fundamentals-and-the-web/08-use-classes-profanity-editor/movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
-    quotes.close()
+    quotes.close
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+        connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+        output = connection.read()
+        print(output)
+        connection.close()
 
 read_text()
