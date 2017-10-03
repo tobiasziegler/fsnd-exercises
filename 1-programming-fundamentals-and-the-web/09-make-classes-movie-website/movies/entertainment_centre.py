3ed32a528b75d12aae1f158e9baa088cbe3818dd
David Mallard <correspondence@davidmallard.id.au>
2017-10-03 21:38:39 +1100

    Complete the Coding The Movie Website exercise

--- 1-programming-fundamentals-and-the-web/09-make-classes-movie-website/movies/entertainment_centre.py
+++ 1-programming-fundamentals-and-the-web/09-make-classes-movie-website/movies/entertainment_centre.py
@@ -1,4 +1,5 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
 "A story of a boy and his toys that come to life",
@@ -20,4 +21,29 @@ deadpool = media.Movie("Deadpool",
 "https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",
 "https://www.youtube.com/watch?v=FyKWUTwSYAs")
 
-deadpool.show_trailer()
+#deadpool.show_trailer()
+
+school_of_rock = media.Movie("School of Rock",
+"Storyline",
+"http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+"https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatoullie",
+"Storyline",
+"https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+"https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+"Storyline",
+"https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+"https://www.youtube.com/watch?v=atLg2wQQxvU")
+
+hunger_games = media.Movie("Hunger Games",
+"Storyline",
+"http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+"https://www.youtube.com/watch?v=PbA63a7H0bo")
+
+movies = [toy_story, avatar, deadpool, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+
+fresh_tomatoes.open_movies_page(movies)
