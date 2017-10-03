2c79f348ab74aecb945f3f8c98ea266300f79e11
David Mallard <correspondence@davidmallard.id.au>
2017-10-03 20:58:01 +1100

    Complete the What Will Be The Output? quiz

--- 1-programming-fundamentals-and-the-web/09-make-classes-movie-website/movies/entertainment_centre.py
+++ 1-programming-fundamentals-and-the-web/09-make-classes-movie-website/movies/entertainment_centre.py
@@ -5,4 +5,11 @@ toy_story = media.Movie("Toy Story",
 "http://upload.wikimedia.org/wikipedia/1/13/Toy_Story.jpg",
 "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+"A marine on an alien planet",
+"http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+"https://www.youtube.com/watch?v=-9ceBgWV8io")
+
+print(avatar.storyline)
