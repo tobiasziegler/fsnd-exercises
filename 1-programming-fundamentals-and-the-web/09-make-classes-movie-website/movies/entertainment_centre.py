1c1bd420d80e9ada00959621c5a753b143920c2f
David Mallard <correspondence@davidmallard.id.au>
2017-10-03 21:23:21 +1100

    Complete the Play Your Favourite Trailer quiz

--- 1-programming-fundamentals-and-the-web/09-make-classes-movie-website/movies/entertainment_centre.py
+++ 1-programming-fundamentals-and-the-web/09-make-classes-movie-website/movies/entertainment_centre.py
@@ -12,5 +12,12 @@ avatar = media.Movie("Avatar",
 "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
 "https://www.youtube.com/watch?v=-9ceBgWV8io")
 
-print(avatar.storyline)
-avatar.show_trailer()
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+deadpool = media.Movie("Deadpool",
+"A fast-talking mercenary with a morbid sense of humor is subjected to a rogue experiment that leaves him with accelerated healing powers and a quest for revenge",
+"https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",
+"https://www.youtube.com/watch?v=FyKWUTwSYAs")
+
+deadpool.show_trailer()
