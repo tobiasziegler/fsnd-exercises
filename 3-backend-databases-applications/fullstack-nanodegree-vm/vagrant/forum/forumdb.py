cbf3db455b4e9722b597255fddfbe48c0ef82a08
David Mallard <correspondence@davidmallard.id.au>
2017-10-16 19:46:06 +1100

    Complete the Give That App A Backend exercise

--- 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/forum/forumdb.py
+++ 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/forum/forumdb.py
@@ -1,15 +1,20 @@
 # "Database code" for the DB Forum.
 
-import datetime
-
-POSTS = [("This is the first post.", datetime.datetime.now())]
+import psycopg2
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  db = psycopg2.connect("dbname=forum")
+  c = db.cursor()
+  c.execute("select content, time from posts order by time desc")
+  posts = c.fetchall()
+  db.close()
+  return posts
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+  db = psycopg2.connect("dbname=forum")
+  c = db.cursor()
+  c.execute("insert into posts values ('%s')" % content)
+  db.commit()
+  db.close()
