bcb4c70d92b9d7a3fce4169ba42e7d31b92e500b
David Mallard <correspondence@davidmallard.id.au>
2017-10-23 13:41:58 +1100

    Complete the Creating a Database - Class and Table exercise

--- 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/restaurant-menu/database_setup.py
+++ 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/restaurant-menu/database_setup.py
@@ -6,5 +6,11 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
