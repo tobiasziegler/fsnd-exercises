0a8cb2e96cc12bc89bfe123ac60f92cf72cd173c
David Mallard <correspondence@davidmallard.id.au>
2017-10-24 11:44:55 +1100

    Complete the Objective 3 quiz

--- 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/restaurant-menu/webserver.py
+++ 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/restaurant-menu/webserver.py
@@ -28,8 +28,46 @@ class WebServerHandler(BaseHTTPRequestHandler):
             output += "</body></html>"
             self.wfile.write(output)
             return
+        if self.path.endswith("/restaurants/new"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            output = ""
+            output += "<html><body>"
+            output += "<h1>Add a New Restaurant</h1>"
+            output += '''<form method='POST' enctype='multipart/form-data'
+                         action='/restaurants/new'><input name="newRestaurantName"
+                         type="text" placeholder="Restaurant Name"><input
+                         type="submit" value="Create"></form>'''
+            output += "</body></html>"
+            self.wfile.write(output)
+            return
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
+
+    def do_POST(self):
+        try:
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+
+                # Get the new name submitted by the form
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                # Create the new restaurant in the database
+                newRestaurant = Restaurant(name = messagecontent[0])
+                session.add(newRestaurant)
+                session.commit()
+
+                # Redirect back to the restaurant list
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
+
+        except:
+            pass
 
 def main():
     try:
