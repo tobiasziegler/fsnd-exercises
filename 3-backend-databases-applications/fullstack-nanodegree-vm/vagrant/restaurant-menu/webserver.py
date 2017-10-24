06343e9a07c8f740441c1a1711c5c60f5b2a75d5
David Mallard <correspondence@davidmallard.id.au>
2017-10-24 11:16:17 +1100

    Complete the Objective 1 quiz

--- 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/restaurant-menu/webserver.py
+++ 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/restaurant-menu/webserver.py
@@ -1,67 +1,33 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path.endswith("/hello"):
+        if self.path.endswith("/restaurants"):
+            restaurants = session.query(Restaurant).all()
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             output = ""
             output += "<html><body>"
-            output += "<h1>Hello!</h1>"
-            output += '''<form method='POST' enctype='multipart/form-data'
-                         action='/hello'><h2>What would you like me to say?
-                         </h2><input name="message" type="text" ><input
-                         type="submit" value="Submit"></form>'''
+            output += "<h1>Restaurants</h1>"
+            for restaurant in restaurants:
+                output += "<p>%s</p>" % restaurant.name
             output += "</body></html>"
             self.wfile.write(output)
-            print output
-            return
-        if self.path.endswith("/hola"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            output = ""
-            output += "<html><body>"
-            output += "<h1>&#161Hola!</h1>"
-            output += '''<form method='POST' enctype='multipart/form-data'
-                         action='/hello'><h2>What would you like me to say?
-                         </h2><input name="message" type="text" ><input
-                         type="submit" value="Submit"></form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
             return
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
-
-    def do_POST(self):
-        try:
-            self.send_response(301)
-            self.end_headers()
-
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-
-            output = ""
-            output += "<html><body>"
-            output += "<h2>Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data'
-                         action='/hello'><h2>What would you like me to say?
-                         </h2><input name="message" type="text" ><input
-                         type="submit" value="Submit"></form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
-
-        except:
-            pass
 
 def main():
     try:
