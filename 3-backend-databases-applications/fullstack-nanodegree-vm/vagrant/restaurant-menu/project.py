84b28d1713a652832892053caa32a434475f6a84
David Mallard <correspondence@davidmallard.id.au>
2017-10-25 09:56:16 +1100

    Complete the Delete Menu Item quiz

--- 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/restaurant-menu/project.py
+++ 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/restaurant-menu/project.py
@@ -47,9 +47,17 @@ def editMenuItem(restaurant_id, menu_id):
         return render_template(
             'editmenuitem.html', restaurant_id=restaurant_id, MenuID=menu_id, item=editedItem)
 
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/',
+           methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+    deletedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        session.delete(deletedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template(
+            'deletemenuitem.html', restaurant_id=restaurant_id, MenuID=menu_id, item=deletedItem)
 
 if __name__ == '__main__':
     app.debug = True
