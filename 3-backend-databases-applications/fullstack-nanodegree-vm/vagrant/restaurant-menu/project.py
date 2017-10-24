e107619b9a4728074abcc16b9bf1b570d69fbc18
David Mallard <correspondence@davidmallard.id.au>
2017-10-25 09:43:03 +1100

    Complete the Form Requests and Redirects exercise

--- 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/restaurant-menu/project.py
+++ 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/restaurant-menu/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -12,15 +12,24 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
-def restaurantMenu():
+def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).first()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     return render_template('menu.html', restaurant = restaurant,
         items = items)
 
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], restaurant_id =
+            restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id =
+            restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id =
+            restaurant_id)
 
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
