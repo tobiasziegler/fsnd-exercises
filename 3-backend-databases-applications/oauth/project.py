e85d118768748cbdb094fd055ce556a47856ff70
David Mallard <correspondence@davidmallard.id.au>
2017-10-26 17:42:09 +1100

    Fix the wrong database filename

--- 3-backend-databases-applications/oauth/project.py
+++ 3-backend-databases-applications/oauth/project.py
@@ -22,7 +22,7 @@ APPLICATION_NAME = "Restaurant Menu Application"
 
 
 #Connect to Database and create database session
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
