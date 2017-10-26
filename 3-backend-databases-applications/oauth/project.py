773d0392d9582e47fab5bf5128e98735ea4fdb12
David Mallard <correspondence@davidmallard.id.au>
2017-10-26 17:07:07 +1100

    Complete the Obtaining Credentials of an Existing User quiz

--- 3-backend-databases-applications/oauth/project.py
+++ 3-backend-databases-applications/oauth/project.py
@@ -111,6 +111,12 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
+    # See if user exists. If it doesn't, make a new one.
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
