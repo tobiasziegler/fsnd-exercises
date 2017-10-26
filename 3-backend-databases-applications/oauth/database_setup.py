2164e966dda9707d352f5c938f959e0bbf73d907
David Mallard <correspondence@davidmallard.id.au>
2017-10-26 16:53:19 +1100

    Complete the Updating the User Model exercise

--- 3-backend-databases-applications/oauth/database_setup.py
+++ 3-backend-databases-applications/oauth/database_setup.py
@@ -2,14 +2,24 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+    picture = Column(String(250))
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -18,10 +28,9 @@ class Restaurant(Base):
            'name'         : self.name,
            'id'           : self.id,
        }
- 
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
-
 
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
@@ -30,7 +39,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
-
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -44,8 +54,7 @@ class MenuItem(Base):
        }
 
 
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 
-engine = create_engine('sqlite:///restaurantmenu.db')
- 
 
 Base.metadata.create_all(engine)
