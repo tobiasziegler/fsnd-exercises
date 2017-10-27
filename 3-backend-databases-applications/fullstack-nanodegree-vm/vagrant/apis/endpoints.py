6fedaac0d38d06607a3df229e7a79091ffaa4b9c
David Mallard <correspondence@davidmallard.id.au>
2017-10-28 10:46:23 +1100

    Complete the Making An Endpoint With Flask quiz

--- 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/apis/endpoints.py
+++ 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/apis/endpoints.py
@@ -3,13 +3,13 @@ app = Flask(__name__)
 # Create the appropriate app.route functions. Test and see if they work
 
 #Make an app.route() decorator here for when the client sends the URI "/puppies"
-
+@app.route('/puppies')
 def puppiesFunction():
     return "Yes, puppies!"
 
 
 #Make another app.route() decorator here that takes in an integer named 'id' for when the client visits a URI like "/puppies/5"
-
+@app.route('/puppies/<int:id>')
 def puppiesFunctionId(id):
   return "This method will act on the puppy with id %s" % id
 
