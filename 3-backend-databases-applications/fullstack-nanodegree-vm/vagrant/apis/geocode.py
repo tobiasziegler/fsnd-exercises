ce5d7d4147672e65bb54541e5cc62fcd678fc796
David Mallard <correspondence@davidmallard.id.au>
2017-10-27 17:24:38 +1100

    Complete the Requesting From Python Code exercise

---
+++ 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/apis/geocode.py
@@ -0,0 +1,15 @@
+import httplib2
+import json
+
+from keys import GOOGLE_API_KEY
+
+def getGeocodeLocation(inputString):
+    key = GOOGLE_API_KEY
+    locationString = inputString.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s' % (
+            locationString, key))
+    h = httplib2.Http()
+    response, content = h.request(url, 'GET')
+    result = json.loads(content)
+    print "response header: %s \n \n" % response
+    return result
