ae156fd42ff84c6c600e0aa146a9d61c551cd1d1
David Mallard <correspondence@davidmallard.id.au>
2017-10-28 12:50:43 +1100

    Complete the Adding Features to your Mashup quiz

--- 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/apis/mashup/views.py
+++ 3-backend-databases-applications/fullstack-nanodegree-vm/vagrant/apis/mashup/views.py
@@ -28,11 +28,44 @@ app = Flask(__name__)
 
 @app.route('/restaurants', methods = ['GET', 'POST'])
 def all_restaurants_handler():
-    #YOUR CODE HERE
+    if request.method == 'GET':
+        restaurants = session.query(Restaurant).all()
+        return jsonify(restaurants = [i.serialize for i in restaurants])
+    elif request.method == 'POST':
+        location = request.args.get('location', '')
+        mealType = request.args.get('mealType', '')
+        restaurant_info = findARestaurant(mealType, location)
+        if restaurant_info != "No Restaurants Found":
+            restaurant = Restaurant(restaurant_name = unicode(restaurant_info['name']), restaurant_address = unicode(restaurant_info['address']), restaurant_image = restaurant_info['image'])
+            session.add(restaurant)
+            session.commit()
+            return jsonify(restaurant = restaurant.serialize)
+        else:
+            return jsonify({"error":"No Restaurants Found for %s in %s" % (mealType, location)})
 
 @app.route('/restaurants/<int:id>', methods = ['GET','PUT', 'DELETE'])
 def restaurant_handler(id):
-    #YOUR CODE HERE
+    restaurant = session.query(Restaurant).filter_by(id=id).one()
+    if request.method == 'GET':
+        return jsonify(restaurant = restaurant.serialize)
+    elif request.method == 'PUT':
+        name = request.args.get('name', '')
+        address = request.args.get('address', '')
+        image = request.args.get('image', '')
+        if name:
+            restaurant.restaurant_name = name
+        if address:
+            restaurant.restaurant_address = address
+        if image:
+            restaurant.restaurant_image = image
+        session.add(restaurant)
+        session.commit
+        return jsonify(restaurant = restaurant.serialize)
+    elif request.method == 'DELETE':
+        session.delete(restaurant)
+        session.commit()
+        return jsonify(restaurant = restaurant.serialize)
+
 
 if __name__ == '__main__':
     app.debug = True
